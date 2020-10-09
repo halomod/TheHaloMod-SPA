f4756123745800223e30f05dfc8c3405f4efcfa3
Trent <trentengelman0@gmail.com>
2020-10-08 23:03:58 -0700

    Incorporate PyTest into basic Flask app

---
+++ server/tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import tempfile
+import pytest
+
+from halomod import create_app
+
+@pytest.fixture
+def app():
+    """Create and configure a new app instance for each test."""
+    app = create_app({"TESTING": True})
+    yield app
+
+@pytest.fixture
+def client(app):
+    "A test client for the app."
+    return app.test_client()
+
---
+++ server/tests/test_halomod.py
@@ -0,0 +1,9 @@
+import os
+import pytest
+
+def test_home(client):
+    response = home(client)
+    assert response.json['start'] == 'This is the HaloModApp'
+
+def home(client):
+    return client.get('/')
