1d86c41a43f73188c0e3448722806a595cdf2327
Alexander Schnapp <a@schnapp.co>
2020-10-10 12:43:55 +0800

    try vue tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{vue}', '!**/node_modules/**', '!<rootDir>/dist/**','!<rootDir>/src/plugins/**', '!<rootDir>/tests/unit/**'],
+  coverageReporters: ['lcov', 'text-summary']
 };
