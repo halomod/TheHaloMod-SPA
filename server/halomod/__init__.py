19576d83d7d08df89a2012ed7431a4391befb30d
Alexander Schnapp <a@schnapp.co>
2020-10-14 13:39:18 +0800

    working ci and dev env setup

--- server/halomod/__init__.py
+++ server/halomod/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     @app.route('/')
     def home():
         return jsonify({"start": 'This is the HaloModApp'})
-    
+
     return app
