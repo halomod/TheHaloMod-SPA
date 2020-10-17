720a48c2b47d837cf84e6780c9bfd9b5dc75e6c9
Trent <trentengelman0@gmail.com>
2020-10-16 21:02:21 -0700

    Conflict resolution

--- server/run.sh
+++ server/run.sh
@@ -6,19 +6,11 @@ for arg in "$@"
 do
     case $arg in
         "--lint" )
-<<<<<<< HEAD
           flake8 halomod_app;;
         "--test" )
           python -m pytest;;
         "--dev" )
           export FLASK_APP=halomod_app
-=======
-          flake8 halomod;;
-        "--test" )
-          python -m pytest;;
-        "--dev" )
-          export FLASK_APP=halomod
->>>>>>> 7923066d6e8465b05dbffe72a21097024bb6f460
           export FLASK_ENV=development
           flask run;;
    esac
