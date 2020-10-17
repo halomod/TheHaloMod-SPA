10eccc1438b36b8753c7736da03e534f932adce5
Trent <trentengelman0@gmail.com>
2020-10-16 20:54:55 -0700

    Task 83 Create /plot endpoint to get figure based on current models [PARTIAL]

--- server/halomod_app/__init__.py
+++ server/halomod_app/__init__.py
@@ -23,4 +23,17 @@ def create_app(test_config=None):
             f.write(buf.getvalue())
         return ""
 
+    @app.route('/plot', methods=["POST"])
+    def plot():
+        request_json = request.get_json()
+        fig_type = request_json["fig_type"]
+        string_models = request_json["models"]
+        models = dict()
+        for label, string_model in string_models:
+            models[label] = json.loads(string_model)
+        img_type = request_json["image_type"]
+        buf, errors = utils.create_canvas(models, fig_type, utils.KEYMAP["fig_type"], img_type)
+        encoding = base64.b64encode(buf.getvalue())
+        return jsonify({"figure": str(encoding)})
+
     return app
