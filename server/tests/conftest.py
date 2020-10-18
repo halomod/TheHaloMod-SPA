4f25b0f8bbef26a0c4cb77538b87f42e4529a440
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-10-18 15:09:51 -0700

    US50T86 Add payload fixture and new plot test
    Right now this saves a file which is the picture that is in the response
    to the root directory. This will be stopped once it comes back
    correctly.

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import pytest
+import json
 
 from halomod_app import create_app
 
@@ -16,3 +17,9 @@ def app():
 def client(app):
     "A test client for the app."
     return app.test_client()
+
+
+@pytest.fixture
+def plot_payload():
+    with open('halomod_app/thm_payload_plot.json') as json_file:
+        return json.load(json_file)
--- server/tests/test_halomod.py
+++ server/tests/test_halomod.py
@@ -1,5 +1,8 @@
 import os
 import pytest
+import json
+import imghdr
+import base64
 
 
 def test_home(client):
@@ -7,9 +10,17 @@ def test_home(client):
     assert response.json['start'] == 'This is the HaloModApp'
 
 
-def test_plot(client):
-    response = client.get('/plot')
-    print(response)
+def test_plot(client, plot_payload):
+    response = client.post('/plot', json=plot_payload)
+    assert response is not None
+    assert response.status_code == 200
+    json_response = response.json
+    assert "figure" in json_response
+    decoded_image = base64.b64decode(json_response['figure'])
+    with open("Output.png", "w") as png_file:
+        png_file.write(json_response['figure'])
+    print(imghdr.what(None, h=decoded_image))
+    assert True is False
 
 
 def home(client):
