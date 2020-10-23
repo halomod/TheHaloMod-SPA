d7eb8fde95675e4cdd209d098fd612097112ec2c
Daniel Blevins <dblevin1@asu.edu>
2020-10-22 22:32:06 -0700

    Task91 Changed html view to support a list

--- client/src/components/MassDefinitionForm.vue
+++ client/src/components/MassDefinitionForm.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="md-layout md-gutter">
-    <div class="md-layout-item">
+  <div class='md-layout md-gutter'>
+    <div class='md-layout-item'>
+      <md-subheader>Mass Definition</md-subheader>
+      <md-divider></md-divider>
       <md-field>
-        <label for="movies">Mass Definition</label>
-        <md-select v-model="MassDefinition" name="MassDefinition" id="MassDefinition" md-dense>
-          <md-option value="None">Use native definition of mass function</md-option>
-          <md-option value="SOMean">Spherical Overdensity wrt mean</md-option>
-          <md-option value="SOCritical">Spherical Overdensity wrt critical</md-option>
-          <md-option value="SOVirial">Virial Spherical Overdensity (Bryan and Norman)</md-option>
-          <md-option value="FOF">Friends-of-Friends</md-option>
+        <label for='MassDefinition'>Mass Definition*</label>
+        <md-select
+          v-model='selectedMassDefinition'
+          name='MassDefinition'
+          id='MassDefinition'
+          md-dense
+        >
+          <md-option
+            v-for='(name, value) in massDefinitionChoices'
+            :key='value'
+            :value='value'
+            >{{ name }}</md-option
+          >
+          <!--<md-option value='None'>Use native definition of mass function</md-option>
+          <md-option value='SOMean'>Spherical Overdensity wrt mean</md-option>
+          <md-option value='SOCritical'>Spherical Overdensity wrt critical</md-option>
+          <md-option value='SOVirial'>Virial Spherical Overdensity (Bryan and Norman)</md-option>
+          <md-option value='FOF'>Friends-of-Friends</md-option>-->
         </md-select>
       </md-field>
     </div>
@@ -16,16 +29,25 @@
 </template>
 
 <script>
-// Sets the default value
+const massDefinitionChoices = {
+  None: 'Use native definition of mass function',
+  SOMean: 'Spherical Overdensity wrt mean',
+  SOCritical: 'Spherical Overdensity wrt critical',
+  SOVirial: 'Virial Spherical Overdensity (Bryan and Norman)',
+  FOF: 'Friends-of-Friends',
+};
+
+// Objects used in the html
 export default {
   name: 'MassDefinitionForm',
   data: () => ({
-    MassDefinition: 'None',
+    massDefinitionChoices,
+    selectedMassDefinition: Object.keys(massDefinitionChoices)[0], // Sets the default value
   }),
 };
 </script>
 
-<style lang="css" scoped>
+<style lang='css' scoped>
 .md-field {
   max-width: 75%;
   margin-left: auto;
