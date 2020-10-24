9a66ae1df5a251a4a54bb74e40d3d9cb7763d2cc
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-10-24 11:57:19 -0700

    T113 Get data binding to work between components

--- client/src/components/CosmologyForm.vue
+++ client/src/components/CosmologyForm.vue
@@ -2,7 +2,7 @@
   <div>
     <md-field>
       <label for="cosmologyChoices">Cosmology</label>
-      <md-select v-model="cosmologyChoice" id="cosmologyChoices">
+      <md-select v-model="cosmologyChoice" id="cosmologyChoices" name="cosmologyChoice">
         <md-option
           v-for="choice in cosmologyChoices"
           :key="choice"
@@ -10,28 +10,39 @@
         >
           {{choice}}
         </md-option>
-        <md-option value="test">
-          Test
-        </md-option>
       </md-select>
     </md-field>
 
-    <md-field>
-      <label>H<sub>0</sub></label>
-      <md-input v-model="h_0" type="number"></md-input>
+<!--
+    <md-field v-for="(input, inputName) in inputs" :key="inputName">
+      <div v-html="input.html"/>
+      <md-input
+        v-model="input.currentValue"
+        type="number"
+        :step="input.step"
+        :min="input.min"
+        :max="input.max"
+      />
+      <span class="md-error">There is an error</span>
     </md-field>
-    <md-field>
-      <label>&#937;<sub>b</sub></label>
-      <md-input v-model="omega_b" type="number"></md-input>
-    </md-field>
-    <md-field>
-      <label>&#937;<sub>m</sub></label>
-      <md-input v-model="omega_m" type="number"></md-input>
-    </md-field>
+    -->
+
+    <FormNumberField
+      v-for="(input, inputName) in inputs"
+      :labelHtml="input.html"
+      :key="inputName"
+      :step="input.step"
+      :currentValue="input.currentValue"
+      :min="input.min"
+      :max="input.max"
+      :setCurrentValue="createSetCurrentValueFunc(inputName)"
+    />
+    <p>The current value of h0 is {{inputs.h_0.currentValue}}</p>
   </div>
 </template>
 
 <script>
+import FormNumberField from './FormNumberField.vue';
 /**
  * The code from the form is below
  * choices = [
@@ -71,6 +82,12 @@
     )
  */
 
+/**
+ * Creates an error message based on the minimum and maximum value.
+ */
+function createErrorMessage(minVal, maxVal) {
+  return `Please enter a value between ${minVal} and ${maxVal}`;
+}
 const cosmologyChoices = [
   'Planck15',
   'Planck13',
@@ -78,18 +95,50 @@ const cosmologyChoices = [
   'WMAP7',
   'WMAP5',
 ];
+const inputs = {
+  h_0: {
+    html: '<label>H<sub>0</sub></label>',
+    currentValue: 11,
+    min: 10,
+    max: 500,
+    step: 1,
+  },
+  omega_b: {
+    html: '<label>&#937;<sub>b</sub></label>',
+    currentValue: 0.009,
+    min: 0.005,
+    max: 0.65,
+    step: 0.001,
+  },
+  omega_m: {
+    html: '<label>&#937;<sub>m</sub></label>',
+    currentValue: 0.3,
+    min: 0.02,
+    max: 2,
+    step: 0.01,
+  },
+};
 export default {
   name: 'CosmologyForm',
   data: () => ({
-    h_0: 0.2,
-    omega_b: 0.3,
-    omega_m: 0.4,
+    inputs,
     cosmologyChoices,
+    createErrorMessage,
     cosmologyChoice: cosmologyChoices[0],
   }),
+  components: {
+    FormNumberField,
+  },
+  methods: {
+    createSetCurrentValueFunc(inputType) {
+      return (newValue) => {
+        this.inputs[inputType].currentValue = newValue;
+      };
+    },
+  },
 };
 </script>
 
 <style>
 
-</style> */
+</style>
---
+++ client/src/components/FormNumberField.vue
@@ -0,0 +1,42 @@
+<template>
+  <md-field>
+    <div v-html="labelHtml"/>
+    <md-input
+      :value="currentValue"
+      v-on:input="handleInput"
+      type="number"
+      :step="step"
+      :min="min"
+      :max="max"
+    />
+    <span class="md-error">There is an error</span>
+  </md-field>
+</template>
+
+<script>
+export default {
+  name: 'FormNumberField',
+  props: {
+    step: Number,
+    min: Number,
+    max: Number,
+    labelHtml: String,
+    currentValue: Number,
+    setCurrentValue: Function,
+  },
+  methods: {
+    handleInput(value) {
+      if (!Number.isNaN(value)) {
+        this.setCurrentValue(Number.parseFloat(value));
+      }
+    },
+    logThisStuff() {
+      console.log('This is a test');
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
