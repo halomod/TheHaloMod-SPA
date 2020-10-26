44c60b6f792fc5d4cf3e5b13438e11fd58561154
Daniel Blevins <dblevin1@asu.edu>
2020-10-25 22:16:25 -0700

    Task97 Growth Form Completed

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <MassDefinitionForm/>
+    <GrowthForm/>
   </div>
 </template>
 
 <script>
 import MassDefinitionForm from '../components/MassDefinitionForm.vue';
+import GrowthForm from '../components/GrowthForm.vue';
 
 export default {
   name: 'Home',
   components: {
     MassDefinitionForm,
+    GrowthForm,
   },
 };
 </script>
