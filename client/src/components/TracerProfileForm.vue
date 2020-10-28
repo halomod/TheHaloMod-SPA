cdcd4cbcb19328c429fb50a8087499f4623bc5db
Tia Peruzzi <tiamariaperuzzi@gmail.com>
2020-10-28 17:27:35 -0400

    Task-132 Create Tracer Profile Form Component
    Add md-select component and bind inputs to tracerProfile model

---
+++ client/src/components/TracerProfileForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <md-field>
+            <label for="tracerProfileChoices">Tracer Profile</label>
+            <md-select v-model="tracerProfile" id="tracerProfileChoices"  name="tracerProfile">
+                <md-option
+                    v-for="choice in tracerProfileChoices"
+                    :key="choice"
+                    :value="choice"
+                >
+                    {{choice}}
+                </md-option>
+            </md-select>
+        </md-field>
+    </div>
+</template>
+
+<script>
+
+const tracerProfileChoices = [
+  'NFW 1997',
+  'Hernquist',
+  'Moore',
+  'Generalized NFW',
+  'Einasto',
+  'Cored NFW',
+];
+
+export default {
+  name: 'TracerProfileForm',
+  data: () => ({
+    tracerProfileChoices,
+    tracerProfile: tracerProfileChoices[0],
+  }),
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <CosmologyForm/>
+    <TracerProfileForm/>
   </div>
 </template>
 
 <script>
 import CosmologyForm from '../components/CosmologyForm.vue';
+import TracerProfileForm from '../components/TracerProfileForm.vue';
 
 export default {
   name: 'Home',
   components: {
     CosmologyForm,
+    TracerProfileForm,
   },
 };
 </script>
