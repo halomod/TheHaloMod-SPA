1e2518a3da053f2aa4083b5a973e77b07786ab59
Tia Peruzzi <tiamariaperuzzi@gmail.com>
2020-10-31 19:02:43 -0400

    Update Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
     <CosmologyForm/>
-    <TracerProfileForm/>
   </div>
 </template>
 
 <script>
 import CosmologyForm from '../components/CosmologyForm.vue';
-import TracerProfileForm from '../components/TracerProfileForm.vue';
 
 export default {
   name: 'Home',
   components: {
     CosmologyForm,
-    TracerProfileForm,
   },
 };
 </script>
