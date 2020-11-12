cd6fe12841d2aa4d7013885b9b87d7c7aca85c95
Tia Peruzzi <tiamariaperuzzi@gmail.com>
2020-11-11 20:10:16 -0500

    Task-161: Refactor to support v-model

--- client/src/components/ProfileForm.vue
+++ client/src/components/ProfileForm.vue
@@ -1,55 +1,74 @@
 <template>
-    <div>
-        <md-field>
-            <label for="profileChoices">Tracer Profile</label>
-            <md-select v-model="tracerChoice" id="tracerProfileChoices"  name="tracerProfile">
-                <md-option
-                    v-for="choice in tracerProfileChoices"
-                    :key="choice"
-                    :value="choice"
-                >
-                    {{choice}}
-                </md-option>
-            </md-select>
-        </md-field>
-
-    </div>
+  <form novalidate>
+    <md-field>
+      <label>Profile</label>
+      <md-select v-model="model.profile_model">
+        <md-option
+          v-for="(value, choice) in choices"
+          :key="choice"
+          :value="value">
+          {{choice}}
+        </md-option>
+      </md-select>
+    </md-field>
+    <double-field
+      v-for="(value, param) in model.profile_params"
+      :value="value"
+      :key="param"
+      :param="param"
+      range=false
+      :placeholder="String(defaults[param])"
+      v-model="model.profile_params[param]"/>
+  </form>
 </template>
 
 <script>
+import DoubleField from './DoubleField.vue';
 import BACKEND_CONSTANTS from '../constants/backend_constants';
 
-const profileChoices = [
-  'NFW 1997',
-  'Hernquist',
-  'Moore',
-  'Generalized NFW',
-  'Einasto',
-  'Cored NFW',
-];
+const profileChoices = {
+  'NFW (1997)': 'NFW',
+  Hernquist: 'Hernquist',
+  Moore: 'Moore',
+  'Generalized NFW': 'GeneralizedNFW',
+  Einasto: 'Einasto',
+  'Cored NFW': 'CoredNFW',
+};
 
 const profileParams = BACKEND_CONSTANTS.Profile_params;
 
 export default {
+  title: 'Profile',
+  id: 'profile',
   name: 'ProfileForm',
+  model: {
+    event: 'onChange',
+    prop: 'parent_model',
+  },
+  props: ['parent_model'],
   data: () => ({
     profileChoices,
     model: {
-      profile_model: tracerProfileChoices[0],
+      profile_model: profileChoices[0],
       profile_params: profileParams.NFW,
     },
-    defaults: profileParms.NFW,
+    defaults: { ...BACKEND_CONSTANTS.Profile_params.NFW },
+    choices: profileChoices,
   }),
-  methods: {
-    updateOptions() {
-      const params = profileParams;
+  updated() {
+    this.$emit('onChange', this.model);
+  },
+  watch: {
+    'model.profile_model': function updateOptions(val) {
       this.model.profile_params = null;
-
-      this.$nextTick(function() {
-        this.model.profile_params = params[this.model.profile_model];
-        this.defaults = profileParams[this.model.profile_model];
+      this.$nextTick(function saveNewOptions() {
+        this.model.profile_params = BACKEND_CONSTANTS.Profile_params[val];
+        this.defaults = BACKEND_CONSTANTS.Profile_params[val];
       });
     },
+  },
+  components: {
+    DoubleField,
   },
 };
 </script>
--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -33,6 +33,7 @@
 import FormWrapper from '@/components/FormWrapper.vue';
 import HaloExclusion from '@/components/HaloExclusion.vue';
 import BiasForm from '@/components/BiasForm.vue';
+import ProfileForm from '@/components/ProfileForm.vue';
 import INITIAL_STATE from '@/constants/initial_state.json';
 
 export default {
@@ -40,6 +41,7 @@ export default {
   components: {
     FormWrapper,
     HaloExclusion,
+    ProfileForm,
   },
   data: () => ({
     params: null,
@@ -57,6 +59,10 @@ export default {
         {
           component: BiasForm,
           model: 'bias',
+        },
+        {
+          component: ProfileForm,
+          model: 'profile',
         },
       ];
       forms.forEach((item) => {
