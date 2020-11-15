b1543c471a665328d820a3d60f4d7ec4be54b927
Alexander Schnapp <a@schnapp.co>
2020-11-15 13:58:26 +0800

    add halo concentration form

--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -8,18 +8,19 @@
       <md-list v-for="(form, index) in forms" :key="index">
         <md-list-item
           :class="{'router-link-active': form.highlight}"
-          v-bind:to="`#${form.component.id}`">
-          {{form.component.title}}
+          v-bind:to="`#${form.props ? form.props.id : form.component.id}`">
+          {{form.props ? form.props.title : form.component.title}}
         </md-list-item>
       </md-list>
     </md-app-drawer>
     <md-app-content>
       <div v-for="(form, index) in forms" :key="index">
         <FormWrapper
-          :name="form.component.title"
-          v-bind:id="`${form.component.id}`"
+          :name="form.props ? form.props.title : form.component.title"
+          v-bind:id="`${form.props ? form.props.id : form.component.id}`"
           @toggle-highlight="(bool) => toggleHighlight(bool, form, index)">
           <component
+            v-bind="form.props"
             :is="form.component"
             v-model="params[form.model]"/>
         </FormWrapper>
@@ -33,7 +34,7 @@
 import clonedeep from 'lodash.clonedeep';
 
 import FormWrapper from '@/components/FormWrapper.vue';
-import TracerConcentration from '@/components/TracerConcentration.vue';
+import Concentration from '@/components/Concentration.vue';
 import HaloExclusion from '@/components/HaloExclusion.vue';
 import BiasForm from '@/components/BiasForm.vue';
 import HMFForm from '@/components/HMFForm.vue';
@@ -44,7 +45,7 @@ export default {
   name: 'Create',
   components: {
     FormWrapper,
-    TracerConcentration,
+    Concentration,
     HaloExclusion,
     HODForm,
     BiasForm,
@@ -59,8 +60,22 @@ export default {
       // make sure you have a "title" and "id" property.
       const forms = [
         {
-          component: TracerConcentration,
+          component: Concentration,
+          model: 'halo_concentration',
+          props: {
+            title: 'Halo Concentration',
+            id: 'halo-concentration',
+            defaultModel: 'Duffy08',
+          },
+        },
+        {
+          component: Concentration,
           model: 'tracer_concentration',
+          props: {
+            title: 'Tracer Concentration',
+            id: 'tracer-concentration',
+            defaultModel: 'Bullock01',
+          },
         },
         {
           component: HaloExclusion,
@@ -85,6 +100,7 @@ export default {
         i.isVisible = false;
       });
       this.forms = forms;
+      this.$forceUpdate();
     },
     toggleHighlight(bool, form, index) {
       const f = form;
@@ -116,7 +132,7 @@ export default {
     handleTopForm(form, index, prefix = '/create') {
       const f = form;
       f.highlight = true;
-      window.history.replaceState({}, '', `${prefix}#${form.component.id}`);
+      window.history.replaceState({}, '', `${prefix}#${form.props ? form.props.id : form.component.id}`);
     },
   },
   created() {
