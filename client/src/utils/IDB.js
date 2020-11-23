1b98663e159fca6439f486812ee455ab5da9a1c5
Alexander Schnapp <a@schnapp.co>
2020-11-23 14:15:50 +0800

    add documentation

--- client/src/utils/IDB.js
+++ client/src/utils/IDB.js
@@ -11,6 +11,7 @@ export default class IDB {
     this.init();
   }
 
+  // initialize db
   init = async () => {
     if (this.db) return;
     this.db = openDB('models', 1, {
@@ -20,31 +21,37 @@ export default class IDB {
     });
   };
 
+  // checks if db is initialized
   check = async () => {
     if (!this.db) throw new Error('DB uninitialized');
     this.db = await this.db;
   };
 
+  // gets item at key from idb
   get = async (key) => {
     await this.check();
     return this.db.get(NAME, key);
   };
 
+  // puts item at key in idb
   put = async (key, value) => {
     await this.check();
     return this.db.put(NAME, value, key);
   };
 
+  // get all items from idb
   getAll = async () => {
     await this.check();
     return this.db.getAll(NAME);
   };
 
+  // get all keys from idb
   keys = async () => {
     await this.check();
     return this.db.getAllKeys(NAME);
   }
 
+  // deletes item at key from idb
   delete = async (key) => {
     await this.check();
     return this.db.delete(NAME, key);
