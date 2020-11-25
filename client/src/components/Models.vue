94f656efa9ed9b0962a1c8cf70cac1aa99905694
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-11-24 17:06:15 -0800

    T177 Build the start of the model UI

---
+++ client/src/components/Models.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <md-toolbar class="md-large">
+      <div class="md-toolbar-row">
+        <div class="md-toolbar-section-start">
+          <h3 class="md-title">Models</h3>
+        </div>
+
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button">
+            <md-icon>refresh</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>more_vert</md-icon>
+          </md-button>
+        </div>
+      </div>
+      <p>
+        paragraph
+      </p>
+
+      <div class="md-toolbar-row">
+        Testing this out
+      </div>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Models',
+  data() {
+    return {
+      stuff: 'something',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="home">
-    You're home!
+  <div class="md-layout md-gutter md-alignment-center">
+    <div class="md-layout-item md-large-size-45 md-small-size-90">
+      <Models/>
+    </div>
+    <div class="md-layout-item md-large-size-45 md-small-size-90">
+      <Models/>
+    </div>
   </div>
 </template>
 
 <script>
+import Models from '../components/Models.vue';
 
 export default {
   name: 'Home',
   // TODO: Make the data at the app level and learn how to pass this to routes
   data: () => ({}),
+  components: {
+    Models,
+  },
 };
 
 </script>
+
+<style scoped>
+  .md-layout-item {
+    margin-top: 16px;
+    margin-bottom: 16px;
+  }
+</style>
