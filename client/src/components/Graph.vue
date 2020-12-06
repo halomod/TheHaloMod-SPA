d83d440d75c6477144456efc8757c1c2eb47fce9
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-12-05 16:03:01 -0800

    T192 Add dropdown for plot type

--- client/src/components/Graph.vue
+++ client/src/components/Graph.vue
@@ -8,17 +8,56 @@
       </div>
       <img v-if="READ_ONLY.plot !== null" :src="READ_ONLY.plot"/>
       <p v-else>No graph has been generated yet</p>
+
+      <md-field v-if="plotChoices">
+        <label for="plotChoices">Plot Type</label>
+        <md-select v-model="plotChoice" id="plotChoices" name="plotChoice">
+          <md-option
+            v-for="choice in plotChoices"
+            :key="choice"
+            :value="choice"
+          >
+            {{choice}}
+
+          </md-option>
+        </md-select>
+      </md-field>
     </md-toolbar>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import baseurl from '@/env';
+
+axios.defaults.withCredentials = true;
+
 export default {
   name: 'Graph',
   data() {
     return {
       READ_ONLY: this.$store.state,
+      plotChoices: null,
+      plotChoice: null,
     };
+  },
+  async created() {
+    let plotChoices = [];
+    try {
+      const result = await axios.get(`${baseurl}/get_plot_types`);
+      const plotTypes = result.data;
+      plotChoices = Object.keys(plotTypes);
+    } catch (error) {
+      // Might be better to tell the user here once the snackbar is ready.
+      console.error(error);
+    }
+    if (plotChoices.includes('dndm')) {
+      this.plotChoice = 'dndm';
+    } else {
+      [this.plotChoice] = plotChoices;
+    }
+
+    this.plotChoices = plotChoices;
   },
 };
 </script>
