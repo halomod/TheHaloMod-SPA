7c6cf0184f8158489ff8572e5039f63716e9fa3e
Trent <trentengelman0@gmail.com>
2021-01-15 15:13:00 -0800

    US171Task202 Test HODForm w/ basic pattern for form testing.

---
+++ client/__tests__/HODForm.spec.js
@@ -0,0 +1,56 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import HODForm from '@/components/HODForm';
+import BACKEND_CONSTANTS from '../src/constants/backend_constants';
+
+describe('Mounted HODForm', () => {
+  const localVue = createLocalVue();
+  const wrapper = mount(HODForm, localVue);
+  
+  const options = Object.keys(BACKEND_CONSTANTS.HOD_params);
+
+  test('has correct default model',() => {
+    expect(wrapper.vm.model.hod_model).toBe('Zehavi05');
+  });
+
+  test('changes model parameters when model is changed',  async () => {
+    for(let option of options){
+      if(wrapper.vm.model.hod_model === option) continue;
+      var oldParams = wrapper.vm.model.hod_params;
+      wrapper.setData({...wrapper.vm.$data, model: {...wrapper.vm.$data.model, hod_model: option}});
+      await localVue.nextTick();
+      await localVue.nextTick();
+      var newParams = wrapper.vm.model.hod_params;
+      if(option == 'Geach12'){
+        expect(JSON.stringify(oldParams)).toBe(JSON.stringify(newParams));
+      } else {
+        expect(JSON.stringify(oldParams)).not.toBe(JSON.stringify(newParams));
+      }
+    };
+  });
+
+  test('renders correct fields for each model selection', async () => {
+    for(let option of options){
+      wrapper.setData({...wrapper.vm.$data, model: {...wrapper.vm.$data.model, hod_model: option}});
+      await localVue.nextTick();
+      await localVue.nextTick();
+      let params = Object.keys(BACKEND_CONSTANTS.HOD_params[option]);
+      for(let param of params){
+        expect(wrapper.html()).toEqual(expect.stringMatching(new RegExp(`.*${param}.*` )));
+      }
+    };
+  });
+
+  test('emits onChange event whenever model selection is changed', async () => {
+    const emitted = wrapper.emitted();
+    let prevCount = 0;
+    for(let option of options){
+      if(wrapper.vm.model.hod_model === option) continue;
+      prevCount = emitted.onChange.length;
+      wrapper.setData({...wrapper.vm.$data, model: {...wrapper.vm.$data.model, hod_model: option}});
+      await localVue.nextTick();
+      await localVue.nextTick();
+      expect(emitted.onChange.length).toBeGreaterThan(prevCount);
+      prevCount = emitted.onChange.length;
+    }
+  })
+})
--- client/jest.config.js
+++ client/jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['**/*.{vue}', '!**/node_modules/**', '!<rootDir>/dist/**', '!<rootDir>/src/plugins/**', '!<rootDir>/tests/unit/**'],
   coverageReporters: ['lcov', 'text-summary'],
+  setupFiles: ['./__tests__/setup.js'],
+  testPathIgnorePatterns: [
+    "<rootDir>/__tests__/setup.js"
+  ],
 };
