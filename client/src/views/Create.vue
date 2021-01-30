7b5f286384e433533fd5eab1ed85e21855d9a572
Trent <trentengelman0@gmail.com>
2021-01-30 11:50:37 -0800

    US221 Add loading indicator during model creation / updating

--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,29 +1,36 @@
 <template>
 <div>
   <Forms :init="initial" v-model="current"/>
-  <div
-  :style="{ position: 'fixed', zIndex: 1, display: 'block', bottom: '10px', right: '10px' }">
-  <md-button @click="showCancelDialog = true" class="md-raised">Cancel</md-button>
-  <md-button @click="showCreateDialog = true" class="md-raised md-primary">Create</md-button>
+  <div id="float">
+    <md-button @click="showCancelDialog = true" class="md-raised">Cancel</md-button>
+    <md-button @click="showCreateDialog = true" class="md-raised md-primary">Create</md-button>
   </div>
-  <md-dialog :md-active.sync="showCreateDialog">
+  <md-dialog v-if="!loading"
+    :md-active.sync="showCreateDialog" >
     <md-dialog-title>Save Model</md-dialog-title>
-    <md-content :style="{margin: '0 15px 0 15px'}">
+    <md-dialog-content>
       <md-field>
         <label>Model Name</label>
         <md-input v-model="name" :value="name"/>
       </md-field>
-    </md-content>
+    </md-dialog-content>
     <md-dialog-actions>
       <md-button @click="save">Save Model</md-button>
       <md-button @click="showCreateDialog = false">Cancel</md-button>
     </md-dialog-actions>
   </md-dialog>
+  <md-dialog v-else
+    :md-active.sync="showCreateDialog"
+    :md-close-on-esc="false"
+    :md-click-outside-to-close="false">
+    <md-dialog-title>Creating your model...</md-dialog-title>
+    <md-dialog-content><md-progress-bar md-mode="indeterminate"/></md-dialog-content>
+  </md-dialog>
   <md-dialog :md-active.sync="showCancelDialog">
     <md-dialog-title>Discard Model</md-dialog-title>
-    <md-content :style="{margin: '0 15px 0 15px'}">
+    <md-dialog-content>
       {{`Are you sure you want to leave the page without creating a new model?`}}
-    </md-content>
+    </md-dialog-content>
     <md-dialog-actions>
       <md-button @click="$router.push('/')">Confirm</md-button>
       <md-button @click="showCancelDialog = false">Cancel</md-button>
@@ -35,7 +42,7 @@
 <script>
 import INITIAL_STATE from '@/constants/initial_state.json';
 import clonedeep from 'lodash.clonedeep';
-import Forms from '@/components/forms';
+import Forms from '@/components/forms/index.vue';
 
 export default {
   name: 'Create',
@@ -65,4 +72,11 @@ export default {
 </script>
 
 <style scoped>
+#float {
+  position: fixed;
+  z-index: 1;
+  display: block;
+  bottom: 10px;
+  right: 10px
+}
 </style>
