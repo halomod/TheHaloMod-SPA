b8c38f942eb1f1d1bce2e42dbe8f0e1185a2315b
Daniel Blevins <dblevin1@asu.edu>
2021-02-08 07:56:40 -0700

    Clean up uneeded file

--- server/dockerStart.py
+++ server/dockerStart.py
@@ -64,6 +64,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-#docker - compose up - -build - -remove - orphans
--- server/dockerStart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-app="thehalomod.server"
-#docker build -t ${app} .
-#docker run -dt -p 5000:5000/tcp ${app}
-docker-compose up --build --remove-orphans
