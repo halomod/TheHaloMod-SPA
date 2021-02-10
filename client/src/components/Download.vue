15f3344ff2baf7c9c16fb8903b4daf13b46c2f1d
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-09 18:51:52 -0800

    T259 Update layout of home to grid
    To help with new button placement

--- client/src/components/Download.vue
+++ client/src/components/Download.vue
@@ -1,10 +1,40 @@
 <template>
-  <button>Download something</button>
+  <md-toolbar class="md-large">
+    <div class="md-toolbar-row">
+      <div class="md-toolbar-section-start">
+        <h3 class="md-title">Download</h3>
+      </div>
+      <md-content>
+        <md-field>
+        <md-select v-model="downloadChoice" id="downloadChoices">
+          <md-option
+            v-for="choice in downloadChoices"
+            :key="choice"
+            :value="choice"
+          >
+            {{choice}}
+          </md-option>
+        </md-select>
+      </md-field>
+      </md-content>
+    </div>
+  </md-toolbar>
 </template>
 
 <script>
+const downloadChoices = [
+  'PDF Thang',
+  'ASCII',
+  'Other stuff',
+];
 export default {
   name: 'Download',
+  data() {
+    return {
+      downloadChoices,
+      downloadChoice: downloadChoices[0],
+    };
+  },
 };
 </script>
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <Navbar/>
-    <div class="md-layout md-gutter md-alignment-top-space-around home">
-      <div class="md-layout-item md-small-size-100">
+    <div class="md-gutter md-alignment-top-space-around home">
+      <div class="md-layout-item md-small-size-100 models">
         <Models/>
       </div>
       <div class="md-layout-item md-small-size-100 graph">
         <Graph/>
       </div>
-      <div class="md-layout-item md-small-size-100">
+      <div class="md-layout-item md-small-size-100 download">
         <Download/>
       </div>
+
     </div>
   </div>
 </template>
@@ -34,15 +35,33 @@ export default {
 </script>
 
 <style scoped>
-  .md-layout-item {
-    margin-top: 16px;
-    margin-bottom: 16px;
+  .home {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "models graph"
+      "download graph"
+      ". graph";
+    column-gap: 16px;
+    row-gap: 16px;
+    margin: 16px;
   }
+  .graph {
+    grid-area: graph;
+  }
+  .models {
+    grid-area: models;
+  }
+  .download {
+    grid-area: download;
+  }
+  /*
   .graph {
     flex-grow: 2;
   }
   .home {
     margin-left: 16px;
     margin-right: 16px;
-  }
+  }*/
 </style>
