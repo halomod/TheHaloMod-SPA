e4622291359d43d787d9fe13cce1c7c2e9a94f86
Alexander Schnapp <a@schnapp.co>
2021-02-12 14:15:38 +0800

    fix expect is not defined error

--- client/tests/immutables.js
+++ client/tests/immutables.js
@@ -3,5 +3,5 @@ import BACKEND_CONSTANTS from '@/constants/backend_constants';
 const constants = JSON.stringify(BACKEND_CONSTANTS);
 
 global.afterEach(() => {
-  expect(JSON.stringify(BACKEND_CONSTANTS)).toBe(constants)
+  expect(JSON.stringify(BACKEND_CONSTANTS)).toBe(constants);
 });
