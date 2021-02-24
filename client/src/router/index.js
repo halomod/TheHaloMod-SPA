ab849b268d9a32764157f45623a0432795b43c87
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-24 06:41:09 -0800

    T268 Revert web chunk

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -58,17 +58,23 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue'),
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
   {
     path: '/create',
     name: 'Create',
-    component: () => import('../views/Forms.vue'),
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Forms.vue'),
   },
   {
     path: '/edit/:id',
     name: 'Edit',
-    component: () => import('../views/Forms.vue'),
+    component: () => import(/* webpackChunkname: "edit" */ '../views/Forms.vue'),
   },
 ];
 
