f7a55256878b6e384d0f68893b6d9f87cccc59a0
Trent <trentengelman0@gmail.com>
2021-02-24 03:19:05 -0800

    US249 Linting

--- client/tests/setup.js
+++ client/tests/setup.js
@@ -5,8 +5,8 @@ import VueObserveVisibility from 'vue-observe-visibility';
 Vue.use(VueMaterial);
 Vue.use(VueObserveVisibility);
 
- // the following IGNORES vue console warnings during testing to avoid repeated warnings
- // that libraries need browser polyfills to function when we're not using a browser */
+// the following IGNORES vue console warnings during testing to avoid repeated warnings
+// that libraries need browser polyfills to function when we're not using a browser */
 global.console = {
   warn: jest.fn(),
   log: console.log,
