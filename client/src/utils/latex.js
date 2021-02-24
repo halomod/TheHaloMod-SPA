0ddbe05008d1843d054ede457cd05a85bd8724a0
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-24 06:37:13 -0800

    T268 Rename latex function

--- client/src/utils/latex.js
+++ client/src/utils/latex.js
@@ -6,7 +6,7 @@ import processLatexString from './stringUtils';
  * @param {string} inputLatex the input string to generate the rendered latex
  * @returns {SVGElement} the rendered SVG element
  */
-const createLatexSvgFromString = (inputLatex) => {
+const latex2svg = (inputLatex) => {
   // Get the MathJax obect, which is inserted in the `public/index.html` file
   const { MathJax } = window;
 
@@ -20,4 +20,4 @@ const createLatexSvgFromString = (inputLatex) => {
     .firstChild;
 };
 
-export default createLatexSvgFromString;
+export default latex2svg;
