a0605fcaec99d38b50d35b53ea0381703ff23ad6
Trent <trentengelman0@gmail.com>
2021-02-24 02:51:53 -0800

    US276 T272 Test DoubleField component

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  testPathIgnorePatterns: [
+    "<rootDir>/tests/unit/.*Form.spec.js",
+  ],
   moduleFileExtensions: ['js', 'json', 'vue'],
   transform: {
     '.*\\.(vue)$': 'vue-jest',
---
+++ client/tests/unit/DoubleField.spec.js
@@ -0,0 +1,82 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import DoubleField from '@/components/DoubleField.vue';
+
+describe('Mounted DoubleField', () => {
+  let wrapper;
+  let localVue;
+  let propsData = {
+    init: 7,
+    range: true,
+    min: 5,
+    max: 10,
+    param: "field_name",
+    html: "<b>field_html<b>",
+  };
+
+  beforeEach(() => {
+    localVue = createLocalVue();
+    wrapper = mount(DoubleField, {
+      localVue,
+      propsData,
+    });
+  });
+
+  test('initializes with correct value', () => {
+    expect(wrapper.vm.current).toEqual(wrapper.vm.init);
+    expect(wrapper.vm.default).toEqual(wrapper.vm.init);
+  });
+
+  test('displays no errors when current is valid', () => {
+    expect(wrapper.html()).toEqual(expect.not.stringContaining('Value must be defined'));
+    expect(wrapper.html()).toEqual(expect.not.stringContaining('Value must be a numeric'));
+    expect(wrapper.html()).toEqual(expect.not.stringContaining('Value must be between 5 and 10'));
+  }),
+
+  test('displays \'not defined\' error if current is not defined', async () => {
+    wrapper.vm.current = '';
+    await localVue.nextTick();
+    expect(wrapper.html()).toEqual(expect.stringContaining('Value must be defined'));
+  });
+
+  test('displays \'not numeric\' error if current is not numeric', async () => {
+    wrapper.vm.current = '0.0.0';
+    await localVue.nextTick();
+    expect(wrapper.html()).toEqual(expect.stringContaining('Value must be a numeric'));
+  });
+
+  test('displays \'not between\' error if current is not in range', async () => {
+    wrapper.vm.current = 12;
+    await localVue.nextTick();
+    expect(wrapper.html()).toEqual(expect.stringContaining('Value must be between 5 and 10'));
+  });
+
+  test('updates current and default when init changes', async () => {
+    await wrapper.setProps({ ...propsData, init: 12 });
+    await localVue.nextTick();
+    expect(wrapper.vm.current).toEqual(wrapper.vm.init);
+    expect(wrapper.vm.default).toEqual(wrapper.vm.init);
+  });
+
+  test('emits current value on valid change', async () => {
+    wrapper.vm.current = 6;
+    await localVue.nextTick();
+    expect(wrapper.emitted('input')[0]).toEqual([6]);
+  });
+
+  test('emits default on invalid change', async () => {
+    wrapper.vm.current = 12;
+    await localVue.nextTick();
+    expect(wrapper.emitted('input')[0]).toEqual([7]);
+  });
+
+  test('displays the field name as text if html prop is undefined', async () => {
+    await wrapper.setProps({ ...propsData, html: undefined });
+    await localVue.nextTick();
+    expect(wrapper.html()).toEqual(expect.stringContaining(wrapper.vm.param));
+  });
+
+  test('displays the field name as html if html prop is defined', async () => {
+    expect(wrapper.html()).toEqual(expect.not.stringContaining(wrapper.vm.param));
+    expect(wrapper.html()).toEqual(expect.stringContaining(wrapper.vm.html));
+  });
+});
