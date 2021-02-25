75b9a5cf1f7d2987842778f829bfb5c509f86b15
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-25 15:37:03 -0800

    T284 Finish the defaults retrieval

--- server/halomod_app/routes/constants.py
+++ server/halomod_app/routes/constants.py
@@ -1,8 +1,9 @@
+from astropy.cosmology.core import Cosmology
 from flask import Blueprint, jsonify
 from halomod import TracerHaloModel
 from hmf._internals import Component, get_base_components
-from hmf.cosmology import Cosmology
 import numpy as np
+from astropy.cosmology.parameters import available as cosmo_options
 from astropy.cosmology import FlatLambdaCDM
 from contextlib import redirect_stdout
 from io import StringIO
@@ -13,11 +14,14 @@ def generate_constants() -> dict:
     """Builds the constants that a front-end can use to build forms or interactive
     pages for the calculations in HMF.
 
+    Note that the constants for `z`, `n`, and `sigma_8` were previously set
+    manually on the front-end. See https://github.com/halomod/TheHaloMod/blob/master/halomod_app/static/halomod_app/js/HideShowRules.js
+
     This should be ran once on server startup."""
 
     backend_constants = {}
 
-    # Capture the no attribute print statements from
+    # Capture the "no attribute" print statements from
     # `get_all_parameter_defaults`.
     throw_away_string = StringIO()
     with redirect_stdout(throw_away_string):
@@ -38,42 +42,38 @@ def generate_constants() -> dict:
             backend_constants[k] = v
 
     # Now find all the parameters for all the component models.
-    for cmp in get_base_components():
-        models = cmp.get_models()
-        this = backend_constants[cmp.__name__ + "_params"] = {}
+    for component in get_base_components():
+        models = component.get_models()
+        this = backend_constants[component.__name__ + "_params"] = {}
 
-        if cmp == Cosmology:
+        if component == Cosmology:
             # Corner case: doesn't have _defaults :-(
             for name in ['Tcmb0', 'Neff', 'm_nu', 'H0', 'Om0']:
                 this[name] = getattr(defaults['cosmo_model'], name)
         else:
             for name, model in models.items():
                 if hasattr(model, "_defaults"):
-                    backend_constants[cmp.__name__ + "_params"][name] = model._defaults
+                    backend_constants[component.__name__ +
+                                      "_params"][name] = model._defaults
 
     # Build the Cosmo defaults, because those are separate
     cosmo_defaults = {}
 
-    # The different cosmological variants in HMF. This could be refactored
-    # somewhere more visible / configurable in case they change. Or pulled
-    # readily from the library somehow.
-    cosmo_choices = [
-        "Planck15",
-        "Planck13",
-        "WMAP9",
-        "WMAP7",
-        "WMAP5"
-    ]
-
     # Build the models so that the constants can be pulled
-    for choice in cosmo_choices:
+    for choice in cosmo_options:
+
+        # The below options do not work with HMF at this time it seems. It
+        # crashes when they process. HMF version 3.3.4
+        if choice == 'Planck18' or choice == 'Planck18_arXiv_v2':
+            continue
         cosmo_model = hmf.cosmo.Cosmology(cosmo_model=getattr(hmf.cosmo, choice))
         cosmo_defaults.setdefault(choice, {
             "H0": cosmo_model.cosmo.H0.value,
             "Ob0": cosmo_model.cosmo.Ob0,
             "Om0": cosmo_model.cosmo.Om0
         })
-        print(cosmo_model)
+
+    backend_constants.setdefault('cosmo_defaults', cosmo_defaults)
 
     return backend_constants
 
@@ -90,6 +90,4 @@ def constants():
     Returns a json representation that holds the constants of HMF.
     """
 
-    return jsonify({
-        "backend_constants": backend_constants
-    })
+    return jsonify(backend_constants)
