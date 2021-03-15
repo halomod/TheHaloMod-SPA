15bd10e88a51aeafaab67ae5d91505dc396a499b
Alexander Schnapp <a@schnapp.co>
2021-03-15 15:18:40 +0800

    add tests back

--- generate_constants.sh
+++ generate_constants.sh
@@ -2,4 +2,4 @@ cd server
 . ./run.sh --install -gc
 cd ..
 mkdir -p ./client/generated
-mv server/backend_constants.json ./client/generated/backend_constants.json
+mv ./server/backend_constants.json ./client/generated/backend_constants.json
