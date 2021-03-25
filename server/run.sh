c816a21b25678fc58c71e4ebe6cc61751771051e
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-03-25 15:07:53 -0700

    T326 Add wheel to run script

--- server/run.sh
+++ server/run.sh
@@ -7,6 +7,7 @@ for arg in "$@"
 do
   case $arg in
     "--install")
+      python3 -m pip install wheel
       python3 -m pip install -r requirements.txt;;
     "--lint" )
       autopep8 -a -r --in-place halomod_app
