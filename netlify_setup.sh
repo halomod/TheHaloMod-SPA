340873c57a13bc5f64a355639672f36f599cc743
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-03-25 12:03:44 -0700

    T326 Add netlify run file.

---
+++ netlify_setup.sh
@@ -0,0 +1,6 @@
+# Copy the requirements from the server file
+cp ./server/requirements.txt ./requirements.txt
+
+# Install the requirements into the python environment
+python3 -m pip install -r requirements.txt
+
