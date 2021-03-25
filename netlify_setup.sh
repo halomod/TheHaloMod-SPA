1919ceed6f931eca9432607d67bc32c50afbaae6
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-03-25 12:07:35 -0700

    T326 Change position of gfortran install

--- netlify_setup.sh
+++ netlify_setup.sh
@@ -1,11 +1,12 @@
 # Copy the requirements from the server file
 cp ./server/requirements.txt ./requirements.txt
 
-# Install the requirements into the python environment
-python3 -m pip install -r requirements.txt
-
 # Setup gfortran
 sudo apt-get install gfortran-8
 mkdir -p gfortran-symlinks
 ln -s /usr/bin/gfortran-8 gfortran-symlinks/gfortran
 export PATH=$PWD/gfortran-symlinks:$PATH
+
+# Install the requirements into the python environment
+python3 -m pip install -r requirements.txt
+
