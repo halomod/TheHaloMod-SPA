ac1c113d6166f987e6c1bfc455930e8c7d987a88
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-03-27 20:15:18 -0700

    T325 Try and fix constant generation

--- generate_constants.sh
+++ generate_constants.sh
@@ -2,9 +2,12 @@
 
 cd server
 sh run.sh --install
+echo "Sleeping for 1 second after install"
+sleep 1
 sh run.sh --generate-constants
 # Sleep for 1 sec to prevent an issue with CI where the backend_constants
 # file didn't exist quiet yet to the script.
+echo "Sleeping for 1 second after constant generation"
 sleep 1
 cd ..
 mkdir -p ./client/generated
