13a5249b85da244ec45be29999a40cd3c6978b96
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-04-02 15:17:12 -0700

    T335 Add report issue button in navbar
    Resolves #137.

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <md-toolbar class="md-primary" md-elevation="1">
         <router-link to="/">
           <md-button class="title-button">
-            <div class="clickable-title">
+            <div class="combined-button">
               <md-avatar>
                 <img class="logo" src="../assets/thm_logo.png">
               </md-avatar>
@@ -13,15 +13,23 @@
 
         <div class="right-side">
 
-          <md-button class="md-icon-button" @click="switchTheme">
-            <md-tooltip>{{themeTooltipText}}</md-tooltip>
-            <md-icon>{{themeIconName}}</md-icon>
+          <md-button
+            href="https://github.com/halomod/TheHaloMod-SPA/issues/new"
+            target="_blank"
+          >
+            <div class="combined-button">
+              <md-icon class="svg-icon" :md-src="require('../assets/github-icon.svg')" />
+              <span>Report Issue</span>
+            </div>
           </md-button>
-
           <router-link to="/acknowledge">
             <md-button class="md-primary">Acknowledge</md-button>
           </router-link>
           <router-link to="/about"><md-button class="md-primary">About</md-button></router-link>
+          <md-button class="md-icon-button" @click="switchTheme">
+            <md-tooltip>{{themeTooltipText}}</md-tooltip>
+            <md-icon>{{themeIconName}}</md-icon>
+          </md-button>
 
         </div>
       </md-toolbar>
@@ -77,12 +85,15 @@ export default {
   .logo {
     margin: 0;
   }
+  .svg-icon {
+    margin-right: 8px;
+  }
   .right-side {
     display: flex;
     flex-direction: row;
     align-items: center;
   }
-  .clickable-title {
+  .combined-button {
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
