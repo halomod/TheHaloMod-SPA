cc3cb9b0e7d15f69f66badde2490366060ab9107
Daniel Blevins <dblevin1@asu.edu>
2021-04-04 03:33:12 -0700

    Remove tests that have been refactored

--- server/tests/test_plot_endpoint.py
+++ server/tests/test_plot_endpoint.py
@@ -11,3 +11,13 @@ def test_get_plot_data(client):
     assert "plot_data" in response.json
     assert "TheModel" in response.json["plot_data"]
 
+# POST
+def test_get_object_data(client):
+    params = ["m", "k", "r", "k_hm"]
+    with client.session_transaction() as sess:
+        sess["models"] = pickle.dumps({"TheModel": TracerHaloModel()})
+    response = client.post('/plot', json={"param_names": params})
+    assert "TheModel" in response.json
+    for param in params:
+        assert param in response.json["TheModel"]
+        assert response.json["TheModel"][param]
