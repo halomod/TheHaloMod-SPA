b6473433b26babee6fe408081de6aa899d41b010
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-04-26 09:35:25 -0700

    Add debugging

--- server/halomod_app/endpoint_model.py
+++ server/halomod_app/endpoint_model.py
@@ -40,6 +40,8 @@ def create():
     models[label] = utils.hmf_driver(
         previous=initial_model,
         **params)  # creates model from params
+    print('The number of models is: ', num_models,
+          ' The length of models is:', len(models))
     if num_models < len(models):
         session["models"] = pickle.dumps(models)  # writes updated model dict to session
     else:
